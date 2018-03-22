59faf5830b5b1c0682e8b3e0e3725d4a3c983da9
Teodor <thelad43@gmail.com>
2018-03-22 08:49:36 +0200

    Added solution for problem 14. Parse JSON Objects

---
+++ 07. JavaScript Syntax and Basic Web - Exercises/14. Parse JSON Objects.js
@@ -0,0 +1,13 @@
+function solve(args) {
+	let array = new Array();
+	for (let string of args) {
+		let object = JSON.parse(string);
+		array.push(object);
+	}
+
+	for (let object of array) {
+		for (let key of Object.keys(object)) {
+			console.log(`${key.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();})}: ${object[key]}`)
+		}
+	}
+}
