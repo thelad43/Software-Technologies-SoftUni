cab1db71e4e09f1059c17e00ac18cd439a35d746
Teodor <thelad43@gmail.com>
2018-03-28 17:28:27 +0300

    Added solution for problem 02. Numbers 1 to 20

---
+++ 10. PHP Syntax and Basic Web/02. Numbers 1 to 20.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Numbers 1 to 20</title>
+</head>
+<body>
+    <ul>
+        <?php
+            for ($i=1; $i <= 20; $i++) {
+                if ($i % 2 == 0) {
+                    $color = "green";
+                }
+                else {
+                    $color = "blue";
+                }
+                
+                echo "<li><span style='color:$color'>$i</span></li>";
+            }
+        ?>
+    </ul>
+</body>
+</html>
