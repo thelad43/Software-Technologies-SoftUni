1431a0106747dc456a193e87779ca0048f210195
Teodor <thelad43@gmail.com>
2018-03-28 17:32:36 +0300

    Added solution for problem 06. Sum Two Numbers

---
+++ 10. PHP Syntax and Basic Web/06. Sum Two Numbers.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sum Two Numbers</title>
+</head>
+
+<body>
+
+<form>
+    <div>First Number:</div>
+    <input type="number" name="num1" />
+    <div>Second Number:</div>
+    <input type="number" name="num2" />
+    <div><input type="submit" /></div>
+</form>
+
+<?php
+    if (isset($_GET['num1']) && isset($_GET['num2'])) {
+        $firstNum = intval($_GET['num1']);
+        $secondNum = intval($_GET['num2']);
+        $result = $firstNum + $secondNum;
+        echo "$firstNum + $secondNum = $result";
+    }
+?>
+
+</body>
+
+</html>
