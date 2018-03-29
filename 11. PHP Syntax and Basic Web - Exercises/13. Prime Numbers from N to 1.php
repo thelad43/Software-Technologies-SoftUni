46201410c3abe96bf13846091fb5e3047d991167
Teodor <thelad43@gmail.com>
2018-03-29 18:56:53 +0300

    Added solution for problem 13. Prime Numbers from N to 1

---
+++ 11. PHP Syntax and Basic Web - Exercises/13. Prime Numbers from N to 1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>First Steps Into PHP</title>
+</head>
+<body>
+    <form>
+        N: <input type="text" name="num" />
+        <input type="submit" />
+    </form>
+	
+    <?php
+        if(isset($_GET['num'])) {
+            $num = intval($_GET['num']);
+            $primeNumbers;
+
+            for ($i = $num ; $i >= 2; $i--) {
+                $isPrime = true;
+
+                for ($j = 2 ; $j <= sqrt($i); $j++) {
+                    if ($i % $j == 0) {
+                        $isPrime = false;
+                    }
+                }
+
+            if ($isPrime) {
+                $primeNumbers[]= $i;
+            }
+        }
+
+        echo join(' ',$primeNumbers);
+    }
+    ?>
+</body>
+</html>
