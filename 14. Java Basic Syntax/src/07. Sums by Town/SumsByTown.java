72d4783657483590087a311de8c9f36dde70fb50
Teodor <thelad43@gmail.com>
2018-04-04 21:36:51 +0300

    Added solution for problem 07. Sums by Town

---
+++ 14. Java Basic Syntax/src/07. Sums by Town/SumsByTown.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class SumsByTown {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TreeMap < String, Double > sumsByTown = new TreeMap < > ();
+
+        int count = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < count; i++) {
+            String[] tokens = scanner.nextLine().split("\\|");
+
+            String town = tokens[0].trim();
+            double income = Double.parseDouble(tokens[1].trim());
+
+            if (sumsByTown.containsKey(town)) {
+                sumsByTown.put(town, sumsByTown.get(town) + income);
+            } else {
+                sumsByTown.put(town, income);
+            }
+        }
+
+        for (String key: sumsByTown.keySet()) {
+            System.out.println(key + " -> " + sumsByTown.get(key));
+        }
+    }
+}
