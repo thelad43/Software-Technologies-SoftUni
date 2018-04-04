c37a364fd3fd223975dbc5647b5fd4c989fa11d1
Teodor <thelad43@gmail.com>
2018-04-04 21:28:33 +0300

    Added solution for problem 01. Expression

---
+++ 14. Java Basic Syntax/src/01. Expression/Expression.java
@@ -0,0 +1,7 @@
+public class Expression {
+    public static void main(String[] args) {
+        Double expression = ((30 + 21) * 0.5 * (35 - 12 - 0.5));
+        Double result = expression * expression;
+        System.out.println(result);
+    }
+}
