b3ec722b86d79e9a04d4c79aaa7aa617deb7ca14
Teodor <thelad43@gmail.com>
2018-04-05 18:32:47 +0300

    Added solution for problem 01. Variable in Hex Format

---
+++ 15. Java Basic Syntax - Exercises/src/01. Variable in Hex Format/VariableInHexFormat.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class VariableInHexFormat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System. in );
+        System.out.println(Integer.parseInt(scanner.nextLine(), 16));
+    }
+}
