a4122a28f8c1f6b1c4ec3688e04b79d7e57d13bc
Teodor <thelad43@gmail.com>
2018-04-16 20:28:40 +0300

    Added solution for problem 01. Numbers from 1-50

---
+++ 18. C# ASP.NET MVC Overview/01. NumbersFrom1To50/NumbersFrom1To50/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿namespace NumbersFrom1To50.Controllers
+{
+    using System.Diagnostics;
+    using Microsoft.AspNetCore.Mvc;
+    using NumbersFrom1To50.Models;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            this.ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            this.ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 18. C# ASP.NET MVC Overview/01. NumbersFrom1To50/NumbersFrom1To50/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NumbersFrom1To50.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
+    }
+}
