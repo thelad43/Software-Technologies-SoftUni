3e179d010ad0171bdc7cb8c10916271038fecea7
Teodor <thelad43@gmail.com>
2018-04-17 19:17:18 +0300

    Added Calculator

---
+++ 19. C# ASP.NET MVC Overview - Exercises/Calculator/CalculatorApp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿namespace CalculatorApp.Controllers
+{
+    using CalculatorApp.Models;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(Calculator calculator)
+        {
+            calculator.CalculateResult();
+            return RedirectToAction("Index", calculator);
+        }
+    }
+}
---
+++ 19. C# ASP.NET MVC Overview - Exercises/Calculator/CalculatorApp/Models/Calculator.cs
@@ -0,0 +1,40 @@
+﻿namespace CalculatorApp.Models
+{
+    using System;
+
+    public class Calculator
+    {
+        public double? LeftOperand { get; set; }
+
+        public double? RightOperand { get; set; }
+
+        public char Operator { get; set; }
+
+        public double? Result { get; set; }
+
+        public void CalculateResult()
+        {
+            switch (this.Operator)
+            {
+                case '+':
+                    this.Result = this.LeftOperand + this.RightOperand;
+                    break;
+
+                case '-':
+                    this.Result = this.LeftOperand - this.RightOperand;
+                    break;
+
+                case '*':
+                    this.Result = this.LeftOperand * this.RightOperand;
+                    break;
+
+                case '/':
+                    this.Result = this.LeftOperand / this.RightOperand;
+                    break;
+
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
+}
